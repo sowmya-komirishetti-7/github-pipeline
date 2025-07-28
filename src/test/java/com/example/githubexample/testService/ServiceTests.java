8f899e970137eb1cb38df1aaf6aca1e36fdcd84d
Sowmya Komirishetty <sowmyakomirishetty09@gmail.com>
2025-07-28 12:57:32 +0530

    added new methods

--- src/test/java/com/example/githubexample/testService/ServiceTests.java
+++ src/test/java/com/example/githubexample/testService/ServiceTests.java
@@ -1,5 +1,6 @@
 package com.example.githubexample.testService;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,5 +13,10 @@ public class ServiceTests {
     void testGreet() {
         String result = service.greet("World");
         assertEquals("Hello, Sowmya", result);
+    }
+
+    @Test
+    void testAddition() {
+        Assertions.assertEquals(4, 2 + 2);
     }
 }
