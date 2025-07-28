fccadc66f460687a0f15c447b5005021d09d3df5
Sowmya Komirishetty <sowmyakomirishetty09@gmail.com>
2025-07-28 12:01:28 +0530

    wrote commit for controller

--- src/main/java/com/example/githubexample/controller/gitHubController.java
+++ src/main/java/com/example/githubexample/controller/gitHubController.java
@@ -3,11 +3,11 @@ package com.example.githubexample.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class gitHubController {
 
+    // this is the controller layer
     @GetMapping("/gitHub")
     public String greet(@RequestParam(defaultValue = "World") String name) {
         return "Hello, " + name + "!";
