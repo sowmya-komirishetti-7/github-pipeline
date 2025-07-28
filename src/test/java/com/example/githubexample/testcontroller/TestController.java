1cdc49ad012e6a3a24ccd539fd0d1db399454aaf
Sowmya Komirishetty <sowmyakomirishetty09@gmail.com>
2025-07-28 15:15:35 +0530

    added new methods

--- src/test/java/com/example/githubexample/testcontroller/TestController.java
+++ src/test/java/com/example/githubexample/testcontroller/TestController.java
@@ -1,13 +1,14 @@
 package com.example.githubexample.testcontroller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(TestController.class)
 public class TestController {
 
     private final MockMvc mockMvc;
